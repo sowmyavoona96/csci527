7255da5a75cf81653d4f3704f6956128b99c009f
sowmyavoona96 <77193876+sowmyavoona96@users.noreply.github.com>
2021-11-18 18:33:09 -0800

    code cleanup:

--- projects/ttagent/Assets/scripts/serveBotScene/ServeBot.cs
+++ projects/ttagent/Assets/scripts/serveBotScene/ServeBot.cs
@@ -9,11 +9,9 @@ public class ServeBot : MonoBehaviour
     Rigidbody ballRB;
     SingleAgentBall ball;
     int score;
-    bool isServe;
 
     void Start()
     {
-        isServe = false;
         Debug.Log("ball starts");
 
         ballRB = ballObj.GetComponent<Rigidbody>();
@@ -22,8 +20,6 @@ public class ServeBot : MonoBehaviour
     }
    public void serveBall() {
         
-        isServe = true;
-
         //Debug.Log("serving ball");
         var ballPos_X = gameObject.transform.position.x;
         var ballPos_Y = init_transform_ball_Y_LB;
@@ -55,30 +51,6 @@ public class ServeBot : MonoBehaviour
     public int getScore()
     {
         return score;
-    }
-
-    public void FixedUpdate()
-    {
-        //Debug.Log("fixed update: " + (isServe?"true" : "false"));
-        if (isServe) {
-            /*
-            Debug.Log("serving ball");
-            var ballPos_X = gameObject.transform.position.x;
-            var ballPos_Y = init_transform_ball_Y_LB;
-            // Random.Range(init_transform_ball_Y_LB, init_transform_ball_Y_UB);
-            var ballPos_Z = Random.Range(init_transform_ball_Z_LB, init_transform_ball_Z_UB);
-
-            //  gameObject.transform.position = new Vector3(gameObject.transform.position.x,
-            //    gameObject.transform.position.y, ballPos_Z);
-
-            ballRB.transform.position = new Vector3(ballPos_X, ballPos_Y,
-                ballPos_Z);
-
-            ballRB.AddForce(Vector3.right * 20f + Vector3.up * 23f);
-            ball.setLastHitAgent(TeamEnum.BOT);
-            */
-            isServe = false;
-        }
     }
 
 }
